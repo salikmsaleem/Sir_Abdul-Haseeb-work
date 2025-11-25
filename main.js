b8f7880cf18adda41dc9a258660205be56157ab9
Salik <salikmsaleem@gmail.com>
2025-11-25 10:09:24 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+    function day() {
+         pass = document.getElementById("pass");
+        if (pass.length>=6) {
+            alert("Correct");
+        }
+        else{
+            alert("Weak Password");
+        }
+        // if (male.checked) {
+        //     console.log(document.getElementById("male").value);
+        // }
+        // if (female.checked) {
+        //     console.log(document.getElementById("female").value);
+        // }
+
+    }
+    alert('hello'); 
+    
