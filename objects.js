d22c3f8a7a62a42af63d52b3ed824ed00a7bf410
salikmsaleem <salikmsaleem@gmail.com>
2025-12-05 09:26:36 +0500

    node

--- objects.js
+++ objects.js
@@ -4,37 +4,38 @@ let obj = {
     lastName : 'Mughal',
     age : 20
 }
-console.log(obj.firstName);
-console.log(obj['lastName']);
+console.log(obj.firstName); //salik
+console.log(obj['lastName']); //mughal
 // updating object properties
 obj.age = 21;
-console.log(obj.age);
+console.log(obj.age); //21
 
 obj.city = 'Karachi';
-console.log(obj.city);
-console.log(obj);
+console.log(obj.city); // adding new property to object
+console.log(obj); // print entire object
 // deleting object properties
 delete obj.age;
-console.log(obj);
+console.log(obj); //age property deleted
 // checking if key exists in object
-console.log('age' in obj); 
+console.log('age' in obj); // false
+console.log('city' in obj); // true
 
 // forlopp to iterate over object keys
 for (let key in obj){
-    console.log(key + ": " + obj[key]);
+    console.log(key + ": " + obj[key]);       //key and value
 }
 //deleting entire object data
 for (let key in obj){
     delete obj[key];
 }
-console.log(obj)
+console.log(obj) //empty object
 //maiking array in object
 let person = {
     name : 'Salik',
     hobbies : ['reading', 'coding', 'gaming']
 }
-console.log(person.hobbies);
-console.log(person.hobbies[1]);
+console.log(person.hobbies); //print entire array
+console.log(person.hobbies[1]); //print specific index of array in object
 
 
 //making nested objects
@@ -46,11 +47,27 @@ let student = {
         age : 21,
         city : 'Lahore'}
 }
-console.log(student.Salik.age);
-console.log(student['Ali']['city']);
+console.log(student.Salik.age);       //20
+console.log(student['Ali']['city']); //Lahore
 //updating nested object property
 student.Ali.age = 22;
-console.log(student.Ali.age);
+console.log(student.Ali.age); //22
 
-// more about objects will be discussed later in detail
-//Aaj k liye bas itna hi milty hen kisi next video
+// object in array
+let employees = [
+    {name : 'Salik', age : 20},
+    {name : 'Ali', age : 21},
+    {name : 'Zafar', age : 22}
+]
+console.log(employees[1].name); //Ali
+console.log(employees[2]['age']); //22
+//updating object property in array
+employees[0].age = 21;
+console.log(employees[0].age); //21
+
+ // Revision
+        // 1> making object
+            const obj = {name: "Salik", age: 20, city: "Karachi"};
+        // 2> adding new property
+            obj.phone = '03193634917';
+            console.log(obj);
